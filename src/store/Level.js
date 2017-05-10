1db1c6e76e02eab8759557c388a7ce87b16759e9
Philip Weaver <simplygreatwork@gmail.com>
2017-05-10 04:57:19 +0000

    Adding initilial implementation for store/Level.

--- src/store/Level.js
+++ src/store/Level.js
@@ -1,9 +1,14 @@
 
+var level = require('level');
+var levelws = require('level-ws');
+
 Store = module.exports = Class.extend({
 	
 	initialize: function(properties) {
 		
 		Object.assign(this, properties);
+		this.database = level('database')
+		this.database = levelws(this.database);
 	},
 	
 	mount: function() {
@@ -17,11 +22,39 @@ Store = module.exports = Class.extend({
 				});
 			}.bind(this),
 			run: function(stream) {
+				stream.request.writeStream = stream.request.writeStream || this.database.createWriteStream();
+				var object = {
+					key : stream.object.key,
+					value : JSON.stringify(stream.object.value)
+				};
+				stream.request.writeStream.write(object);
+				stream.push({
+					put : stream.object
+				});
+				stream.next();
+			}.bind(this),
+			ending : function(stream) {
+				if (stream.request.writeStream) stream.request.writeStream.end();
+				stream.next();
+			}
+		});
+		
+		this.connection.mount({					// translate the query for simplified processing
+			id: 'store-get-optimize-query',
+			before : 'store-get',
+			on: function(request) {
+				request.accept({
+					topic: 'store',
+					action: 'get'
+				});
+			}.bind(this),
+			run: function(stream) {
+				stream.push(stream.object);
 				stream.next();
 			}.bind(this)
 		});
 		
-		this.connection.mount({
+		this.connection.mount({					// get every and pass on for filtering
 			id: 'store-get',
 			on: function(request) {
 				request.accept({
@@ -30,32 +63,71 @@ Store = module.exports = Class.extend({
 				});
 			}.bind(this),
 			run: function(stream) {
+				stream.push(stream.object);
+				stream.next();
+			}.bind(this)
+		});
+		
+		this.connection.mount({					// begin by supporting where:{id:'xxxx'}
+			id: 'store-get-where',
+			after : 'store-get',
+			on: function(request) {
+				request.accept({
+					topic: 'store',
+					action: 'get'
+				});
+			}.bind(this),
+			run: function(stream) {
+				stream.push(stream.object);
+				stream.next();
+			}.bind(this)
+		});
+		
+		this.connection.mount({					// needs to collect all then sort
+			id: 'store-get-sort',
+			after : 'store-get-where',
+			on: function(request) {
+				request.accept({
+					topic: 'store',
+					action: 'get'
+				});
+			}.bind(this),
+			run: function(stream) {
+				stream.push(stream.object);
 				stream.next();
 			}.bind(this)
 		});
 		
 		this.connection.mount({
-			id: 'store-recent',
+			id: 'store-get-range',				// e.g. range is the same as limit:10 {0-9}
+			after : 'store-get-sort',
 			on: function(request) {
 				request.accept({
 					topic: 'store',
-					action: 'recent'
+					action: 'get'
 				});
 			}.bind(this),
 			run: function(stream) {
+				var range = {
+					begin : 0,
+					end : 9
+				};
+				stream.push(stream.object);
 				stream.next();
 			}.bind(this)
 		});
 		
-		this.connection.mount({
-			id: 'store-find',
+		this.connection.mount({					// only return specific fields: include or exclude
+			id: 'store-get-fields',
+			after : 'store-get-range',
 			on: function(request) {
 				request.accept({
 					topic: 'store',
-					action: 'find'
+					action: 'get'
 				});
 			}.bind(this),
 			run: function(stream) {
+				stream.push(stream.object);
 				stream.next();
 			}.bind(this)
 		});
